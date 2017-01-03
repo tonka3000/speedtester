855c389081cc17bcb45d82b6977fbbc5dc940d2a
tonka3000 <tonka3100@gmail.com>
2017-01-03 21:34:28 +0100

    make speedtest and html generation deactivatable

--- speed.py
+++ speed.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os
+import os, argparse
 from app.speed import getDownloadSpeed
 from app.conv import convertToHtml
 from datetime import datetime
@@ -10,24 +10,31 @@ import config
 __dir = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='speedtester')
+    parser.add_argument('--no-speedtest', help='skip the speedtest', action='store_true')
+    parser.add_argument('--no-html', help='skip the html generation', action='store_true')
+    args = parser.parse_args()
+
     data_directory = os.path.abspath(config.data_directory)
     if not os.path.exists(data_directory):
         os.makedirs(data_directory)
 
-    logfile = os.path.join(data_directory, "log.txt")
+    if not args.no_speedtest:
+        logfile = os.path.join(data_directory, "log.txt")
 
-    now = datetime.now()
-    ts = now.strftime("%Y-%m-%d_%H-%M-%S")
+        now = datetime.now()
+        ts = now.strftime("%Y-%m-%d_%H-%M-%S")
 
-    ts = str(now)
-    download_speed, upload_speed = getDownloadSpeed()
-    end = datetime.now()
-    duration = end - now
-    print("download speed: " + str(download_speed))
-    print("upload speed: " + str(upload_speed))
+        ts = str(now)
+        download_speed, upload_speed = getDownloadSpeed()
+        end = datetime.now()
+        duration = end - now
+        print("download speed: " + str(download_speed))
+        print("upload speed: " + str(upload_speed))
 
-    with open(logfile, "a") as f:
-        f.write(download_speed + ":" + upload_speed + "\t" + ts + "\t" + str(duration) + "\n")
+        with open(logfile, "a") as f:
+            f.write(download_speed + ":" + upload_speed + "\t" + ts + "\t" + str(duration) + "\n")
 
-    print("convert log to html")
-    convertToHtml()
+    if not args.no_html:
+        print("convert log to html")
+        convertToHtml()
